8453a6c8e7ad2618436e966adeec3ccbbceda34c
M Huxain <h.xain@yahoo.com>
2016-09-29 11:15:43 +0500

    updates

--- 6-mobx-react/src/js/TodoList.js
+++ 6-mobx-react/src/js/TodoList.js
@@ -41,3 +41,43 @@ export default class TodoList extends React.Component {
 
   }
 }
+
+export class Menu extends React.Component {
+
+  Todos = this.props.store;
+
+  toggleComplete(todo) {
+    todo.complete = !todo.complete;
+  }
+
+  filter(e) {
+    this.props.store.filter = e.target.value;
+  }
+  render() {
+    const { filter, filteredTodos, todos, clearComplete } = this.props.store;
+    const todoList = filteredTodos.map(
+      (todo, i) => (
+        <li key={todo.id}>{todo.value}
+          <input type="checkbox" value={todo.complete} onChange={this.toggleComplete.bind(this, todo) } checked={todo.complete} />
+          <input type="button" onClick={() => this.Todos.delete(todo) } value="X" />
+        </li>
+      )
+    );
+
+    return(
+      <div>
+        <h1>MobX2</h1>
+        <b>{filter}</b><br></br>
+        Create new: <input className="create" ref="createNewInput"  />
+      <button onClick={(e) => {this.Todos.create(this.refs.createNewInput.value); this.refs.createNewInput.value = "";}}>Create</button><br /><br />
+        Search: <input className="filter" value={filter} onChange={(e) => this.filter(e)} /> <br />
+        <ul>
+            {todoList}
+
+        </ul>
+        <button onClick={clearComplete}>Clear Complete</button>
+
+      </div>);
+
+  }
+}
--- 6-mobx-react/src/js/main.js
+++ 6-mobx-react/src/js/main.js
@@ -5,6 +5,11 @@ import ReactDOM from "react-dom"
 import TodoList from "./TodoList"
 import TodoStore from "./TodoStore"
 
-const app = document.getElementById("app")
+function el(id) { return document.getElementById( id ); }
 
+const app = el("app")
+const menu = el("menu")
+
+
+ReactDOM.render(<TodoList store={TodoStore} />, menu)
 ReactDOM.render(<TodoList store={TodoStore} />, app)
